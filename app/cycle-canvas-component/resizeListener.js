d176e0662c13c2be0cf65cd4b9694843bb0c686c
Fuunnx <geraud.henrion@gmail.com>
2016-12-21 23:12:51 +0100

    Make resize handler more consistent
    Sometimes waiting for next event loop was not enough and an error was throwned because contentDocument was null

--- app/cycle-canvas-component/resizeListener.js
+++ app/cycle-canvas-component/resizeListener.js
@@ -1,7 +1,11 @@
+import insertionQ from 'insertion-query'
+
 export const addResizeListener = function (element, onResize) {
   const resizeHandler = () => element.dispatchEvent(new CustomEvent('resize', {bubbles: true}))
   const obj = document.createElement('object')
+  const objID = String(new Date + Math.round(Math.random() * 1000))
   obj.type = 'text/html'
+  obj.setAttribute('id', objID)
   obj.setAttribute('style', `\
 display: block;\
 position: absolute;\
@@ -14,8 +18,8 @@ pointer-events: none;\
 z-index: -1;\
   `)
   element.appendChild(obj)
-
-  setImmediate(() => {
+  insertionQ('#' + objID)
+  .every(() => {
     const computedStyle = getComputedStyle(element)
     if (computedStyle.position === 'static') {
       element.style.position = 'relative'
