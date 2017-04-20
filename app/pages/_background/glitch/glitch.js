0517cb73f71e48f7fbdd236cf1a1ac5917f65e28
Fuunnx <geraud.henrion@gmail.com>
2017-04-20 14:33:04 +0200

    Reduce size of glitches and use canvas-components new pixelRatio utility

--- app/pages/_background/glitch/glitch.js
+++ app/pages/_background/glitch/glitch.js
@@ -1,11 +1,20 @@
 import {canvas} from 'app/cycle-canvas-component'
-import {rect} from 'cycle-canvas'
 import {vnode} from 'utils/vnode'
-import xs from 'xstream'
+import {rect} from 'cycle-canvas'
 import {map} from 'ramda'
+import xs from 'xstream'
 
 export default function Glitch ({DOM, Animation}) {
-  return vnode(({selector, attributes}) => {
+  return vnode(({selector, attributes: attributes_}) => {
+    const pixelRatio = 1
+    const attributes = {
+      ...attributes_,
+      attrs: {
+        ...attributes_.attrs,
+        'data-pixel-ratio': pixelRatio,
+      },
+    }
+
     const resize$ = DOM.select(selector)
       .events('resize')
       .map(x => x.target)
@@ -16,15 +25,14 @@ export default function Glitch ({DOM, Animation}) {
       .startWith({width: 320, height: 480})
 
     const state$ = resize$
-      .map(({width, height}) => model({width, height}))
+      .map(({width, height}) => model({width, height, pixelRatio}))
 
-    // const Animation = xs.periodic(80)
     const counter$ = Animation.fold((x) => x+1, 0)
       .filter(x => x%5 == 0)
       .map(x => x/5)
-    const nodes$ = xs.combine(state$, counter$)
-      .map(([fn, n]) => fn(Math.sin(n/5) * Math.sin(n/3) * 10))
 
+    const nodes$ = xs.combine(state$, counter$)
+      .map(([fn, n]) => fn(Math.sin(n/5) * Math.sin(n/3) * 3))
 
     const view$ = nodes$
       .map(map(x => rect({
@@ -34,25 +42,28 @@ export default function Glitch ({DOM, Animation}) {
       .map(xs => rect({}, xs))
       .map(view => canvas(selector, attributes, [view]))
 
-
     return view$
   })
 }
 
-const model = ({width, height}) => (multiplier) => {
-  return [...Array(10)].map(() => randomRect({width, height}, multiplier))
-}
+const model = ({width, height, pixelRatio}) => (multiplier) => (
+  [...Array(15)].map(() => randomRect({width, height, pixelRatio}, multiplier))
+)
 
-function randomRect ({width, height}, multiplier) {
+function randomRect ({width, height, pixelRatio}, multiplier) {
   const width_ = Math.round(Math.random() * 15 * multiplier + 1)
   const height_ = Math.round(Math.random() * 3 * multiplier + 2 - width_ / 15)
-  const x = Math.round(Math.random() * width)
-  const y = Math.round(Math.random() * height)
+  const x = Math.round(Math.random() * width * pixelRatio)
+  const y = Math.round(Math.random() * height * pixelRatio)
   const transformations = [
-      {translate: {x, y}},
+    {translate: {x, y}},
   ]
+
   return {
-    x: Math.round(-width_ / 2),
-    y: Math.round(-height_ / 2),
-    width: width_, height: height_, transformations}
+    x: Math.round(-width_ / 2 * pixelRatio),
+    y: Math.round(-height_ / 2 * pixelRatio),
+    width: width_ * pixelRatio,
+    height: height_ * pixelRatio,
+    transformations,
+  }
 }
